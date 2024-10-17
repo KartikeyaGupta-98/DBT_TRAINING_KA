fb3d374d6184ee245f66338c80689ef1a091dbfa
Kartikeya Gupta Alapati <kartikeyaa@systechusa.com>
2024-10-17 07:21:50 +0000

    commit

--- models/DBT_EXERCISES/pre_post_hook.sql
+++ models/DBT_EXERCISES/pre_post_hook.sql
@@ -7,10 +7,11 @@
         "INSERT INTO {{ this.schema }}.audit_log (action, model_name, timestamp) VALUES ('start', '{{ this.name }}', CURRENT_TIMESTAMP)"
     ],
     post_hook=[
-        "INSERT INTO {{ this.schema }}.audit_log (action, model_name, timestamp) VALUES ('end', '{{ this.name }}', CURRENT_TIMESTAMP)"
+        "INSERT INTO {{ this.schema }}.audit_log (action, model_name, timestamp) VALUES ('end', '{{ this.name }}', CURRENT_TIMESTAMP)",
+        "UPDATE {{ this .schema}}.pre_post_hook SET bond_duration_date = DATEADD(year, 2, HIRE_DATE)"
     ]
 ) }}
-select * from TRAINING.DBT_KALAPATI.DBT_SNAP_TIME
+select *,null as bond_duration_date from TRAINING.DBT_KALAPATI.DBT_SNAP_TIME
 {% if is_incremental() %}
 -- Incremental logic to get only new records
 WHERE HIRE_DATE > (SELECT MAX(HIRE_DATE) FROM {{ this }})
